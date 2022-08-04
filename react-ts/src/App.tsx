70e793c29d559be33d701cf8dc5a804fc0992dd6
Jean <chakib3500@gmail.com>
2022-08-04 16:16:34 +0200

    state

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useState } from 'react';
 import InputField from './InputField';
 
 
 const App:React.FC = ()=> {
   
-  const [state,setState] = useState('');
+  const [state,setState] = useState<string>('');
+  const [count,setCount] = useState<number | null | undefined>(1);
+  const [obj,setObj] = useState<{text:string}>({text:'hello'});
+
+  useEffect(()=>{
+    setCount(null);
+    setCount(undefined);
+    setObj({text:'hello world'});
+  },[])
+
 
   return (
     <div className="App">
       <h1>React with Typescript</h1>
-      <InputField value={state} setValue={(e: React.ChangeEvent<HTMLInputElement>)=>{
-        setState(e.currentTarget.value)
-      }}/>
+      <InputField 
+        value={state} 
+        setValue={(e: React.ChangeEvent<HTMLInputElement>)=>setState(e.currentTarget.value)}
+        user={{username:'jean',email:'jean@email.com'}}
+      />
     </div>
   );
 }
--- react-ts/src/InputField.tsx
+++ react-ts/src/InputField.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
 
+interface User
+{
+  username:string,
+  email:string,
+  age?:number,
+}
+
 interface Props
 {
   value:string
   setValue:(e: React.ChangeEvent<HTMLInputElement>)=>void
+  user:User,
 }
 
 const InputField:React.FC<Props> = ({value,setValue}) => {
